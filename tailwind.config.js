61d33d5dcd032ee89a1f36225eb4e0adf79bcd1c
PJ-Stone50 <pj.webdeveloper01@gmail.com>
2023-12-23 15:28:54 +0700

    Update UX/UI Header,Features,My works

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
+        primary: "#DEF37F",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
         "wd-darkgrey": "#2D2D2D",
@@ -12,7 +12,7 @@ module.exports = {
       },
       backgroundImage: {
         "hero-pattern":
-          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
+          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/imgs/hero-section.png')",
       },
       fontFamily: {'sans':['poppins','Arial','sans-serif']}
     },
